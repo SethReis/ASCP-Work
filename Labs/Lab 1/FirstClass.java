b9a4fa7f63a33e4747a4bb275423191ede16f720
unknown <sreis@WS-I3-16.WS-ACADBUS.EDU>
2016-08-25 08:42:46 -0700

    First Commit

---
+++ Labs/Lab 1/FirstClass.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class FirstClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirstClass
+{
+    private int a;
+    private int b;
+    
+    public FirstClass()
+    {
+        // initialise instance variables
+        a = 2;
+        b = 5;
+    }
+
+    public int addEmUp()
+    {
+        // put your code here
+        return a + b;
+    }
+    
+    public int difference ()
+    {
+        return a - b;
+    }
+}
---
+++ Labs/Lab 3/FactCalcRunner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class FactCalcRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FactCalcRunner
+{
+    public static void main(String[] args) {
+        int n1 = 0;
+        
+        FactCalc fc = new FactCalc();
+        System.out.println("Number = " + n1);
+        System.out.println("The factorial of " + n1 + " is: " + fc.factorial(n1));
+    }
+}
